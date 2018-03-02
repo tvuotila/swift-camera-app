c975aa921a4fc10f463a64fc1f34dacdbd6a2c5c
Tero Vuotila <tero.vuotila@gmail.com>
2018-03-02 19:20:25 +0200

    Working app

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -8,10 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func foo() {
+        let imagePicker = UIImagePickerController();
+        if UIImagePickerController.availableCaptureModes(for: .front) == nil {
+            let alert = UIAlertController(title: "No camera", message: "BLAABLAA", preferredStyle: .alert);
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil);
+            alert.addAction(okAction);
+            present(alert, animated: true, completion: nil);
+            return
+        }
+        imagePicker.delegate = self;
+        imagePicker.sourceType = .camera;
+        imagePicker.cameraCaptureMode = .photo;
+        imagePicker.cameraDevice = .front;
+
+        self.present(imagePicker, animated: true, completion: nil);
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        cameraButton.layer.cornerRadius = 0.5 * cameraButton.bounds.size.width;
+        imageView.layer.cornerRadius = 10;
+        imageView.clipsToBounds = true;
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight, .flexibleBottomMargin, .flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin];
+        imageView.contentMode = .scaleAspectFit
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +45,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil);
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            return
+        }
+        imageView.image = image;
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    }
 
 }
 
